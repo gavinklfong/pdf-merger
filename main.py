0ab4d7d5db1ffecdc578585171e60d09aa63a700
Gavin Fong <gavinklfong@gmail.com>
2026-01-31 13:49:42 +0000

    update about dialog

--- main.py
+++ main.py
@@ -118,14 +118,31 @@ class MainWindow(QWidget):
         size.moveCenter(screen.center())
         self.move(size.topLeft())
 
+    # def showAboutDialog(self):
+    #         QMessageBox.information(
+    #         self,
+    #         "About PDF Merger",
+    #         (
+    #             "PDF Merger\n\nA simple tool to merge and compress PDF files.\n\n",
+    #             "This application uses PySide6 (Qt for Python),\n"
+    #             "licensed under the LGPL v3.\n\n"
+    #         ),
+    #         QMessageBox.Close
+    #         )
+
+
     def showAboutDialog(self):
         QMessageBox.information(
             self,
             "About PDF Merger",
-            "PDF Merger\n\nA simple tool to merge and compress PDF files.",
+            (
+                "PDF Merger\n\n"
+                "A simple tool to merge and compress PDF files.\n\n"
+                "This application uses PySide6 (Qt for Python),\n"
+                "licensed under the LGPL v3.\n\n"
+            ),
             QMessageBox.Close
         )
-
 
     # ---------------------------------------------------------
     # External viewer
